ae761e03813df1f43b89841a22030981ad7290bc
cameronmetcalfe <cameronmetcalfe@hotmail.com>
2020-06-30 20:14:40 -0700

    Added code skeleton to the web app, added new pages for creating listings and buying things

--- WebApp/testPage.go
+++ WebApp/testPage.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", HelloServer)
+	http.HandleFunc("/list", ListingPage)
+	http.HandleFunc("/add-listing", AddListing)
+	http.HandleFunc("/purchase", PurchasePage)
+	http.HandleFunc("/bought", BuyListing)
+
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -16,4 +22,22 @@ func HelloServer(w http.ResponseWriter, r *http.Request) {
 
 	t, _ := template.ParseFiles("testPage.html")
 	t.Execute(w, nil)
+}
+
+func ListingPage(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("testListing.html")
+	t.Execute(w, nil)
+}
+
+func AddListing(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Add listing function called")
+}
+
+func PurchasePage(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("testPurchase.html")
+	t.Execute(w, nil)
+}
+
+func BuyListing(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Buying function called")
 }
